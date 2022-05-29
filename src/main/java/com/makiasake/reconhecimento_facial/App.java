9aac1b5d1a5aa300ab33ca52706db7fb46e8d0e8
makiasake <alexandre.maki@hotmail.com>
2022-05-29 15:20:49 -0300

    Initial project setup

---
+++ src/main/java/com/makiasake/reconhecimento_facial/App.java
@@ -0,0 +1,13 @@
+package com.makiasake.reconhecimento_facial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
